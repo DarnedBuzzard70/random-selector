2523378f02cc787666a8331797806b0f379bfba4
张语诚 <76857649+DarnedBuzzard70@users.noreply.github.com>
2023-05-20 22:17:01 +0800

    Add files via upload
    Upload the source file.

---
+++ launcher.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+inline int read_int() {
+	int s=0,w=1;
+	char ch=getchar();
+	while(ch<'0'||ch>'9') {
+		if(ch=='-') {
+			w=-1;
+		}
+		ch=getchar();
+	}
+	while(ch>='0'&&ch<='9') {
+		s=10*s+ch-'0';
+		ch=getchar();
+	}
+	return s*w;
+}
+int main() {
+	srand(time(0));
+	system("start main.exe");
+	return 0;
+}
